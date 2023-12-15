883268adeebbea8494f8f3c9b237125086adb42f
Pavlo Kostushevych <46346552+rynikk21@users.noreply.github.com>
2023-12-15 18:30:34 +0100

    commit

---
+++ goit_project/goit_project/main.py
@@ -0,0 +1,56 @@
+
+class PersonalAssistant:
+    def __init__(self):
+        self.contacts = []
+        self.notes = []
+
+    def task_1(self):
+        1+1
+
+    def task_2(self):
+        2+2
+
+    def task_3(self):
+        3+3
+
+    def task_4(self):
+        4+4
+
+    def task_5(self):
+        5+5
+
+    def task_6(self):
+        6+6
+
+    def task_7(self):
+        7+7
+
+    def task_8(self):
+        8+8
+
+    def task_9(self):
+        9+9
+
+    def task_10(self):
+        10+10
+
+    def task_11(self):
+        11+11
+
+
+def main():
+    assistant = PersonalAssistant()
+    while True:
+        print("\nВведіть команду (для допомоги введіть 'help'):")
+        command = input("> ").lower()
+
+        if command == 'help':
+            print("Доступні команди: exit")
+        elif command == 'exit':
+            print("До побачення!")
+            break
+        else:
+            print("Невідома команда. Введіть 'help' для списку доступних команд.")
+
+if __name__ == "__main__":
+    main()
