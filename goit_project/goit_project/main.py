48b770aba41d6b98f1771504b67754c19658b371
Zpfamily <65131398+Zpfamily@users.noreply.github.com>
2023-12-16 21:40:39 +0200

    Update main.py

--- goit_project/goit_project/main.py
+++ goit_project/goit_project/main.py
@@ -1,5 +1,9 @@
 
 from datetime import datetime,timedelta
+import re
+from pathlib import Path
+import shutil
+import sys
 
 class Field():
     def __init__(self, value):
@@ -114,8 +118,169 @@ class PersonalAssistant:
     def task_10(self):
         10+10
 
-    def task_11(self):
-        11+11
+        #  task_11(self):
+class CleanFolder():
+    CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+    TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+    trans = dict()
+
+    for cyrillic, latin in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+        trans[ord(cyrillic)] = latin
+        trans[ord(cyrillic.upper())] = latin.upper()
+        
+    def normalize(name: str) -> str:
+        translate_name = re.sub(r'\W', '_', name.translate(CleanFolder.trans))
+        translate_name = ".".join(translate_name.rsplit("_",1))
+        return translate_name
+
+    #--------------------------------------------------
+    JPEG_IMAGES = []
+    PNG_IMAGES=[]
+    JPG_IMAGES = []
+    SVG_IMAGES = []
+    AVI_VIDEO = []
+    MP4_VIDEO = []
+    MOV_VIDEO = []
+    MKV_VIDEO = []
+    DOC_DOCUMENTS = []
+    DOCX_DOCUMENTS = []
+    TXT_DOCUMENTS = []
+    PDF_DOCUMENTS = []
+    XLSX_DOCUMENTS = []
+    PPTX_DOCUMENTS = []
+    MP3_AUDIO = []
+    OGG_AUDIO = []
+    WAV_AUDIO = []
+    AMR_AUDIO = []
+    ARCHIVES = []
+    MY_OTHERS = []
+
+    REGISTER_EXTENSION = {
+        'JPEG': JPEG_IMAGES,
+        'PNG': PNG_IMAGES,
+        'JPG': JPG_IMAGES,
+        'SVG': SVG_IMAGES,
+        'AVI': AVI_VIDEO,
+        'MP4': MP4_VIDEO,
+        'MOV': MOV_VIDEO,
+        'MKV': MKV_VIDEO,
+        'DOC': DOC_DOCUMENTS,
+        'DOCX': DOCX_DOCUMENTS,
+        'TXT': TXT_DOCUMENTS,
+        'PDF': PDF_DOCUMENTS,
+        'XLSX': XLSX_DOCUMENTS,
+        'PPTX': PPTX_DOCUMENTS,
+        'MP3': MP3_AUDIO,
+        'OGG': OGG_AUDIO,
+        'WAV': WAV_AUDIO,
+        'AMR': AMR_AUDIO,
+        'ZIP': ARCHIVES,
+        'GZ': ARCHIVES,
+        'TAR': ARCHIVES,
+    }
+
+    FOLDERS = []
+    EXTENSIONS = set()
+    UNKNOWN = set()
+
+    def get_extension(name: str) -> str:
+        return Path(name).suffix[1:].upper()
+
+    def scan(folder: Path):
+        for item in folder.iterdir():
+            # Робота з папкою
+            if item.is_dir():  # перевіріямо чи обєкт папка
+                if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                    CleanFolder.FOLDERS.append(item)
+                    CleanFolder.scan(item)
+                continue
+            
+            # Робота з файлами                    
+            extension = CleanFolder.get_extension(item.name) # беремо розширення файлу
+            full_name = folder / item.name # беремо повний шлях до файлу
+            if not extension:
+                CleanFolder.MY_OTHERS.append(full_name)
+            else:
+                try:
+                    ext_reg = CleanFolder.REGISTER_EXTENSION[extension]
+                    ext_reg.append(full_name)
+                    CleanFolder.EXTENSIONS.add(extension)
+                except KeyError:
+                    CleanFolder.UNKNOWN.add(extension)
+                    CleanFolder.MY_OTHERS.append(full_name)
+
+
+    def handle_media(file_name: Path, target_folder: Path):
+        target_folder.mkdir(exist_ok=True, parents=True)
+        file_name.replace(target_folder / CleanFolder.normalize(file_name.name))
+        
+    def handle_documents(file_name: Path, target_folder: Path):
+        target_folder.mkdir(exist_ok=True, parents=True)
+        file_name.replace(target_folder / CleanFolder.normalize(file_name.name))
+        
+    def handle_archive(file_name: Path, target_folder: Path):
+        target_folder.mkdir(exist_ok=True, parents=True)
+        folder_for_file = target_folder / CleanFolder.normalize(file_name.name.replace(file_name.suffix, ''))
+        folder_for_file.mkdir(exist_ok=True, parents=True)
+        try:
+            shutil.unpack_archive(str(file_name.absolute()), str(folder_for_file.absolute()))
+        except shutil.ReadError:
+            folder_for_file.rmdir()
+            return
+        file_name.unlink()
+
+    def main(folder: Path):
+        CleanFolder.scan(folder)
+        for file in CleanFolder.JPEG_IMAGES:
+            CleanFolder.handle_media(file, folder / 'images' / 'JPEG')
+        for file in CleanFolder.JPG_IMAGES:
+            CleanFolder.handle_media(file, folder / 'images' / 'JPG')  
+        for file in CleanFolder.PNG_IMAGES:
+            CleanFolder.handle_media(file, folder / 'images' / 'PNG')
+        for file in CleanFolder.SVG_IMAGES:
+            CleanFolder.handle_media(file, folder / 'images' / 'SVG') 
+        for file in CleanFolder.AVI_VIDEO:
+            CleanFolder.handle_media(file, folder / 'video' / 'AVI')
+        for file in CleanFolder.MP4_VIDEO:
+            CleanFolder.handle_media(file, folder / 'video' / 'MP4')
+        for file in CleanFolder.MOV_VIDEO:
+            CleanFolder.handle_media(file, folder / 'video' / 'MOV')
+        for file in CleanFolder.MKV_VIDEO:
+            CleanFolder.handle_media(file, folder / 'video' / 'MKV')
+        for file in CleanFolder.DOC_DOCUMENTS:
+            CleanFolder.handle_documents(file, folder / 'documents' / 'DOC')
+        for file in CleanFolder.DOCX_DOCUMENTS:
+            CleanFolder.handle_documents(file, folder / 'documents' / 'DOCX')
+        for file in CleanFolder.TXT_DOCUMENTS:
+            CleanFolder.handle_documents(file, folder / 'documents' / 'TXT')
+        for file in CleanFolder.PDF_DOCUMENTS:
+            CleanFolder.handle_documents(file, folder / 'documents' / 'PDF') 
+        for file in CleanFolder.XLSX_DOCUMENTS:
+            CleanFolder.handle_documents(file, folder / 'documents' / 'XLSX')
+        for file in CleanFolder.PPTX_DOCUMENTS:
+            CleanFolder.handle_documents(file, folder / 'documents' / 'PPTX')
+        for file in CleanFolder.MP3_AUDIO:
+            CleanFolder.handle_media(file, folder / 'audio' / 'MP3')
+        for file in CleanFolder.OGG_AUDIO:
+            CleanFolder.handle_media(file, folder / 'audio' / 'OGG')
+        for file in CleanFolder.WAV_AUDIO:
+            CleanFolder.handle_media(file, folder / 'audio' / 'WAV')
+        for file in CleanFolder.AMR_AUDIO:
+            CleanFolder.handle_media(file, folder / 'audio' / 'AMR') 
+        for file in CleanFolder.MY_OTHERS:
+            CleanFolder.handle_media(file, folder / 'MY_OTHER')   
+                
+        for file in CleanFolder.ARCHIVES:
+            CleanFolder.handle_archive(file, folder / 'ARCHIVES') 
+        
+        for folder in CleanFolder.FOLDERS[::-1]:
+            # видаляємо пусті папки після сортування
+            try:
+                folder.rmdir()
+            except OSError:
+                print(f'Error during remove: {folder}')
+    
 
 
 
@@ -133,6 +298,15 @@ def main():
             assistant.add_contact()
         elif command == "birthday_day":
             assistant.get_birthdays()
+        elif command.lower() == 'clean':
+            path = input("Write a path: ")
+            
+            if Path(path).exists():
+                folder_process = Path(path).resolve()
+                CleanFolder.main(folder_process)
+                print("Done")
+            else: 
+                print("You wrote a wrong directory")
         elif command == 'exit':
             print("До побачення!")
             break
